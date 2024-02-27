458217a67c183704782eeb2bce4c99c021388c08
littleBitsman <adittyanischal@hotmail.com>
2024-02-27 17:05:18 -0500

    a

--- main.rs
+++ main.rs
@@ -1,182 +1,222 @@
-extern crate websocket;
 extern crate execute;
 extern crate json;
+extern crate websocket;
 
-use std::io::stdin;
+use core::time;
 use std::sync::mpsc::channel;
-use std::thread;
+use std::thread::{self, sleep};
 
+use json::object;
 use websocket::client::ClientBuilder;
+use websocket::header::Headers;
 use websocket::{Message, OwnedMessage};
 
-use std::process::Command;
 use execute::Execute;
+use std::process::Command;
 
 const CONNECTION: &'static str = "ws://127.0.0.1:2794";
-
-#[allow(dead_code)]
 enum LEDCommand {
-	Red,
-	Green,
-	Blue,
-	Purple,
-	Violet,
-	Teal,
-	Yellow,
-	White,
-	Off,
-	Clownbarf,
-	Blink,
-	Hold
+    Red,
+    Green,
+    Blue,
+    Purple,
+    Violet,
+    Teal,
+    Yellow,
+    White,
+    Off,
+    Clownbarf,
+    Blink,
+    Hold,
 }
 
 impl std::fmt::Display for LEDCommand {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		match self {
-			LEDCommand::Red => write!(f, "red"),
-			LEDCommand::Green => write!(f, "green"),
-			LEDCommand::Blue => write!(f, "blue"),
-			LEDCommand::Purple => write!(f, "purple"),
-			LEDCommand::Violet => write!(f, "purple"),
-			LEDCommand::Teal => write!(f, "teal"),
-			LEDCommand::Yellow => write!(f, "yellow"),
-			LEDCommand::White => write!(f, "white"),
-			LEDCommand::Off => write!(f, "off"),
-			LEDCommand::Clownbarf => write!(f, "clownbarf"),
-			LEDCommand::Blink => write!(f, "blink"),
-			LEDCommand::Hold => write!(f, "hold")
-		}
-	}
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LEDCommand::Red => write!(f, "red"),
+            LEDCommand::Green => write!(f, "green"),
+            LEDCommand::Blue => write!(f, "blue"),
+            LEDCommand::Purple => write!(f, "purple"),
+            LEDCommand::Violet => write!(f, "purple"),
+            LEDCommand::Teal => write!(f, "teal"),
+            LEDCommand::Yellow => write!(f, "yellow"),
+            LEDCommand::White => write!(f, "white"),
+            LEDCommand::Off => write!(f, "off"),
+            LEDCommand::Clownbarf => write!(f, "clownbarf"),
+            LEDCommand::Blink => write!(f, "blink"),
+            LEDCommand::Hold => write!(f, "hold"),
+        }
+    }
 }
 
 fn set_color(arg: LEDCommand) {
-	let mut cmd = Command::new("/usr/local/lb/LEDcolor/bin/setColor");
-	cmd.arg(arg.to_string());
-	let _ = cmd.execute_check_exit_status_code(0);
+    let mut cmd = Command::new("/usr/local/lb/LEDcolor/bin/setColor");
+    cmd.arg(arg.to_string());
+    cmd.execute_check_exit_status_code(0).unwrap()
 }
 
 fn get_input() -> u8 {
-	let mut cmd = Command::new("/usr/local/lb/ADC/bin/getADC");
-	cmd.arg("-1");
-	match cmd.execute_output() {
-		Ok(output) => output.stdout[0],
-		Err(_) => 0
-	}
+    let mut cmd = Command::new("/usr/local/lb/ADC/bin/getADC");
+    cmd.arg("-1");
+    match cmd.execute_output() {
+        Ok(output) => output.stdout[0],
+        Err(_) => 0,
+    }
+}
+
+fn set_output(value: u8) {
+    let mut cmd = Command::new("/usr/local/lb/DAC/bin/setDAC");
+    cmd.arg(format!("{:04x}", value));
 }
 
 fn main() {
-	println!("Connecting to {}", CONNECTION);
+    set_color(LEDCommand::Green);
+    set_color(LEDCommand::Blink);
+    loop {
+        let t = thread::spawn(start);
+        match t.join() {
+            Ok(()) => {}
+            Err(_) => {
+                set_color(LEDCommand::Red);
+                set_color(LEDCommand::Blink);
+                sleep(time::Duration::from_secs(2));
+                set_color(LEDCommand::Green);
+            }
+        }
+    }
+}
 
-	let client = ClientBuilder::new(CONNECTION)
-		.unwrap()
-		.connect_insecure()
-		.unwrap();
+fn start() {
+    println!("Connecting to {}", CONNECTION);
 
-	println!("Successfully connected");
+    let mut headers = Headers::new();
+    headers.append_raw("MAC-Address", "test".into());
+    headers.append_raw("User-Agent", "littleARCH cloudBit".into());
 
-	let (mut receiver, mut sender) = client.split().unwrap();
+    let client = ClientBuilder::new(CONNECTION)
+        .unwrap()
+        .custom_headers(&headers)
+        .connect_insecure()
+        .unwrap();
 
-	let (tx, rx) = channel();
+    println!("Successfully connected");
 
-	let tx_1 = tx.clone();
+    set_color(LEDCommand::Hold);
 
-	let send_loop = thread::spawn(move || {
-		loop {
-			// Send loop
-			let message = match rx.recv() {
-				Ok(m) => m,
-				Err(e) => {
-					println!("Send Loop: {:?}", e);
-					return;
-				}
-			};
-			match message {
-				OwnedMessage::Close(_) => {
-					let _ = sender.send_message(&message);
-					// If it's a close message, just send it and then return.
-					return;
-				}
-				_ => (),
-			}
-			// Send the message
-			match sender.send_message(&message) {
-				Ok(()) => (),
-				Err(e) => {
-					println!("Send Loop: {:?}", e);
-					let _ = sender.send_message(&Message::close());
-					return;
-				}
-			}
-		}
-	});
+    let mut current_input: u8 = 0;
 
-	let receive_loop = thread::spawn(move || {
-		// Receive loop
-		for message in receiver.incoming_messages() {
-			let message = match message {
-				Ok(m) => m,
-				Err(e) => {
-					println!("Receive Loop: {:?}", e);
-					let _ = tx_1.send(OwnedMessage::Close(None));
-					return;
-				}
-			};
-			match message {
-				OwnedMessage::Close(_) => {
-					// Got a close message, so send a close message and return
-					let _ = tx_1.send(OwnedMessage::Close(None));
-					return;
-				}
-				OwnedMessage::Ping(data) => {
-					match tx_1.send(OwnedMessage::Pong(data)) {
-						// Send a pong in response
-						Ok(()) => (),
-						Err(e) => {
-							println!("Receive Loop: {:?}", e);
-							return;
-						}
+    let (mut receiver, mut sender) = client.split().unwrap();
+
+    let (tx, rx) = channel();
+
+    let tx_1 = tx.clone();
+
+    let send_loop = thread::spawn(move || {
+        loop {
+            // Send loop
+            let message = match rx.recv() {
+                Ok(m) => m,
+                Err(e) => {
+                    println!("Send Loop: {:?}", e);
+                    return;
+                }
+            };
+            match message {
+                OwnedMessage::Close(_) => {
+                    let _ = sender.send_message(&message);
+                    // If it's a close message, just send it and then return.
+                    return;
+                }
+                _ => (),
+            }
+            // Send the message
+            match sender.send_message(&message) {
+                Ok(()) => (),
+                Err(e) => {
+                    println!("Send Loop: {:?}", e);
+                    let _ = sender.send_message(&Message::close());
+                    return;
+                }
+            }
+        }
+    });
+
+    let receive_loop = thread::spawn(move || {
+        // Receive loop
+        for message in receiver.incoming_messages() {
+            let message = match message {
+                Ok(m) => m,
+                Err(e) => {
+                    println!("Receive Loop: {:?}", e);
+                    let _ = tx_1.send(OwnedMessage::Close(None));
+                    return;
+                }
+            };
+            match message {
+                OwnedMessage::Close(_) => {
+                    // Got a close message, so send a close message and return
+                    let _ = tx_1.send(OwnedMessage::Close(None));
+                    return;
+                }
+                OwnedMessage::Ping(data) => {
+                    match tx_1.send(OwnedMessage::Pong(data)) {
+                        // Send a pong in response
+                        Ok(()) => (),
+                        Err(e) => {
+                            println!("Receive Loop: {:?}", e);
+                            return;
+                        }
+                    }
+                }
+                OwnedMessage::Text(data) => {
+                    let r = json::parse(&data);
+                    if !r.is_ok() {
+                        return;
+                    }
+					let parsed = r.unwrap();
+                    if !parsed.is_object() {
+                        return;
+                    }
+
+					match parsed {
+						json::JsonValue::Object(obj) => {
+							if obj["opcode"] == 0x2 { // OUTPUT
+								let new = obj["data"]["value"].as_u8().expect("bad output packet from server");
+								set_output(new);
+							} else if obj["opcode"] == 0x3 {
+								println!("received Hello packet")
+							}
+						},
+						_ => {}
 					}
-				}
-				// Say what we received
-				_ => println!("Receive Loop: {:?}", message)
-			}
-		}
-	});
+                }
+                _ => {}
+            }
+        }
+    });
 
-	loop {
-		let mut input = String::new();
+    loop {
+        let right_now = get_input();
+        if right_now != current_input {
+            current_input = right_now;
+            let success = tx
+                .send(OwnedMessage::Text(json::stringify(object!{
+                    opcode: 0x1,
+                    data: object! {
+                        value: current_input
+                    }
+                })))
+                .is_err();
+            if !success {
+                break;
+            }
+        }
+    }
 
-		stdin().read_line(&mut input).unwrap();
+    println!("connection closed");
 
-		let trimmed = input.trim();
+    let _ = send_loop.join();
+    let _ = receive_loop.join();
 
-		let message = match trimmed {
-			"/close" => {
-				// Close the connection
-				let _ = tx.send(OwnedMessage::Close(None));
-				break;
-			}
-			// Send a ping
-			"/ping" => OwnedMessage::Ping(b"PING".to_vec()),
-			// Otherwise, just send text
-			_ => OwnedMessage::Text(trimmed.to_string()),
-		};
-
-		match tx.send(message) {
-			Ok(()) => (),
-			Err(e) => {
-				println!("Main Loop: {:?}", e);
-				break;
-			}
-		}
-	}
-
-	// We're exiting
-
-	println!("Waiting for child threads to exit");
-
-	let _ = send_loop.join();
-	let _ = receive_loop.join();
-
-	println!("Exited");
+    println!("Exiting")
 }
