3e7f9d91e233eab3ccffe028712e5d4ec5ce86bd
littleBitsman <adittyanischal@hotmail.com>
2024-03-03 19:15:13 +0000

    build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-cargo build --release --target armv5te-unknown-linux-gnueabi --static
-mv target/armv5te-unknown-linux-gnueabi/release/cloud_client ./
+cargo build --release --target armv5te-unknown-linux-musleabi --static
+mv target/armv5te-unknown-linux-musleabi/release/cloud_client ./
 
 # commit
 git commit -a -m "build"
