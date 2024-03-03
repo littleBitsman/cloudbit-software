a50d5274737e0475bddab43845dd0fe07ed43665
littleBitsman <adittyanischal@hotmail.com>
2024-03-03 22:59:27 +0000

    build

--- src/main.rs
+++ src/main.rs
@@ -132,7 +132,7 @@ fn start(conf: CloudClientConfig) {
 
     let mut current_input: u8 = 0;
     let request = Request::get(&conf.cloud_url)
-        .header("MAC-Address", conf.mac_address.as_str())
+        // .header("MAC-Address", conf.mac_address.as_str())
         // .header("CB-Id", conf.cb_id.as_str())
         .header("User-Agent", "littleARCH cloudBit")
         .header("Host", url.host_str().unwrap())
