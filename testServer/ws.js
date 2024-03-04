09906908ae21d1ab760eb977e6990d8369e5b00a
littleBitsman <adittyanischal@hotmail.com>
2024-03-03 19:33:16 -0500

    gah

--- testServer/ws.js
+++ testServer/ws.js
@@ -4,19 +4,23 @@ const server = new ws.Server({
     port: 3000
 })
 
+server.broadcast = function(d) {
+    server.clients.forEach(c => c.send(d))
+}
+
 server.on('connection', (c, r) => {
     console.log('connect')
-    c.on('message', d => console.log(d.toString('utf8')))
+    c.on('message', d => console.log(`d: ${d.toString('utf8')}`))
 })
 
 process.stdin.on('data', d => {
     const num = parseInt(d.toString('utf8'))
     if (isNaN(num) || num < 0 || num > 0xFFFF) return
     
-    server.clients.forEach(v => v.send(JSON.stringify({
+    server.broadcast(JSON.stringify({
         opcode: 0x2,
         data: {
             value: num
         }
-    })))
+    }))
 })
