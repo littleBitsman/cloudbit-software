77c6391a05d35b3c9199dc88fc2fca91cf04b99a
littleBitsman <adittyanischal@hotmail.com>
2024-08-02 20:49:14 -0400

    add & correct license notices

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-// cloudbit-software © 2024 by littleBitsman is licensed under CC BY-NC-SA 4.0.
-// To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/
+// This file is part of cloudbit-software.
+//
+// cloudbit-software - an alternative software for the littleBits cloudBit.
+// 
+// Copyright (C) 2024 littleBitsman
+// 
+// cloudbit-software is free software: you can redistribute it and/or modify 
+// it under the terms of the GNU General Public License as published by 
+// the Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// cloudbit-software is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License 
+// along with this program. If not, see https://www.gnu.org/licenses/.
 
 const DEFAULT_URL: &'static str = "wss://gateway.cloudcontrol.littlebitsman.dev/";
 
