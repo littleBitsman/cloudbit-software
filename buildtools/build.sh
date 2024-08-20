35aeb3d403ae91acbe3b6122cc14e84e7d484d38
littleBitsman <littlebitsman@littlebitsman.dev>
2024-08-20 14:19:02 +0000

    chore: change build.sh

--- buildtools/build.sh
+++ buildtools/build.sh
@@ -8,8 +8,3 @@ git pull
 # build
 # apparently gnueabi uses local system deps., and musleabi doesn't so I'm using that now :)
 cross build --release --target armv5te-unknown-linux-musleabi
-mv target/armv5te-unknown-linux-musleabi/release/cloud_client ./
-
-# commit
-git commit -a -m "build"
-git push
